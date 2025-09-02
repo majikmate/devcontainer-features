45e3341abb2a7dbbdc62a52cb4be93f3077f2e0f
Hannes Stauss <scalarion@nimblescape.com>
2025-09-02 17:23:21 +0200

    fixed setting locales

--- src/locales/install.sh
+++ src/locales/install.sh
@@ -14,10 +14,38 @@ apt-get update
 apt-get install -y tzdata
 apt-get install -y locales
 
+ensure_locale() {
+    _locale=$1
+    if [ -z "${_locale}" ]; then
+        echo "ensure_locale: No locale provided."
+        return 1
+    fi
+
+    _locale_underscore=$(echo "${_locale}" | sed 's/-/_/')
+    _locale_entry="${_locale_underscore} UTF-8"
+
+    if grep -q -E "^\s*${_locale_entry}\s*$" /etc/locale.gen; then
+        echo "Locale ${_locale_underscore} already enabled."
+    elif grep -q -E "^\s*#\s*${_locale_entry}\s*$" /etc/locale.gen; then
+        echo "Enabling locale ${_locale_underscore}."
+        sed -i -E "s/^\s*#\s*${_locale_entry}\s*$/${_locale_entry}/" /etc/locale.gen
+    else
+        echo "Adding locale ${_locale_underscore} to /etc/locale.gen."
+        echo "${_locale_entry}" >> /etc/locale.gen
+    fi
+}
+
+ensure_locale "$LANG"
+ensure_locale "$TIME"
+ensure_locale "$NUMERIC"
+ensure_locale "$MONETARY"
+ensure_locale "$MEASUREMENT"
+
+dpkg-reconfigure -f noninteractive locales
+update-locale LANG="$LANG" LC_TIME="$TIME" LC_NUMERIC="$NUMERIC" LC_MONETARY="$MONETARY" LC_MEASUREMENT="$MEASUREMENT"
+
 ln -fs "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
-
-update-locale LANG="$LANG" LC_TIME="$TIME" LC_NUMERIC="$NUMERIC" LC_MONETARY="$MONETARY" LC_MEASUREMENT="$MEASUREMENT"
 
 # The 'install.sh' entrypoint script is always executed as the root user.
 #
