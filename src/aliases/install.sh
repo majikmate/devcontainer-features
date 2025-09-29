0c1241940215379513f8e585189cbb8130712fab
Hannes Stauss <scalarion@nimblescape.com>
2025-09-29 16:32:39 +0200

    Remove unnecessary blank line before parse_aliases function

--- src/aliases/install.sh
+++ src/aliases/install.sh
@@ -11,7 +11,6 @@ echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_
 echo "The effective dev container containerUser is '$_CONTAINER_USER'"
 echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
 
-
 # Parse the comma-separated aliases string and create alias commands
 parse_aliases() {
     if [ -z "$ALIASES" ]; then
